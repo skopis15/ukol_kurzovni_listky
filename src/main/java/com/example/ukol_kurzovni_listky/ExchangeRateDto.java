12f0f9bd122a0c952a6c355b4957de201044c60c
Radim <skopis15@gmail.com>
2021-09-19 11:08:17 +0200

    ll

--- src/main/java/com/example/ukol_kurzovni_listky/ExchangeRateDto.java
+++ src/main/java/com/example/ukol_kurzovni_listky/ExchangeRateDto.java
@@ -1,7 +1,6 @@
 package com.example.ukol_kurzovni_listky;
 
 import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
